5c19d8542f4047ffad47f4770a2299e439b25ff1
uyamanaka <ushio.yamanaka@hacomono.co.jp>
2023-07-24 16:44:37 +0900

    Revert "chore: support for es2015 (e.g. legacy webpack, node version) (#14)"
    This reverts commit 202c978baa30717db679cd637121670fa8e5598e.

--- test/run.mjs
+++ test/run.mjs
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-import { jsonFusion } from 'json-fusion/dist/es2015/index.js'
+import { jsonFusion } from '../dist/index.js'
 
 async function main() {
   const result = await jsonFusion('./fixture/basic', { cwd: __dirname, output: '.output/output-esm.json' })
